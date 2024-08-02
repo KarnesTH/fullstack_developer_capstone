d26127141dde8dc656a37712394c97463090d00b
Patrick Hähnel <p_haehnel@hotmail.de>
2024-08-02 15:15:50 +0200

    change lint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,6 +13,7 @@ from .restapis import get_request, analyze_review_sentiments, post_review
 # Get an instance of a logger
 logger = logging.getLogger(__name__)
 
+
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
@@ -149,7 +150,7 @@ def get_dealer_details(request, dealer_id):
 
 # Create a `add_review` view to submit a review
 def add_review(request):
-    if request.user.is_anonymous == False:
+    if request.user.is_anonymous is False:
         data = json.loads(request.body)
         try:
             response = post_review(data)
@@ -157,7 +158,8 @@ def add_review(request):
                 "status": 200,
                 "review": response
             })
-        except:
+        except Exception as err:
+            print(f"Error: {err}")
             return JsonResponse({
                 "status": 401,
                 "message": "Error in posting review"
