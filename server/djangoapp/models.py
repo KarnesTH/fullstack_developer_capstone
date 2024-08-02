3a11706dc1545de29f8909c40648001a9bfd16f1
Patrick Hähnel <p_haehnel@hotmail.de>
2024-08-02 15:05:20 +0200

    upade lint

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -11,7 +11,10 @@ class CarMake(models.Model):
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    car_make = models.ForeignKey(
+        CarMake,
+        on_delete=models.CASCADE
+    )
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -20,7 +23,11 @@ class CarModel(models.Model):
         ('SPORTCAR', 'Sportcar'),
         ('SUPERSPORTLER', 'Supersportler'),
     ]
-    car_type = models.CharField(max_length=20, choices=CAR_TYPES, default="SUV")
+    car_type = models.CharField(
+        max_length=20,
+        choices=CAR_TYPES,
+        default="SUV"
+    )
     year = models.IntegerField(
         default=2023,
         validators=[MaxValueValidator(2023), MinValueValidator(2015)]
