3bbb3fef1bf6e1df0c06d509869aaa4a93c98ccd
Patrick Hähnel <p_haehnel@hotmail.de>
2024-08-03 00:24:29 +0200

    edit typos

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -3,5 +3,5 @@
 echo "Making migrations and migrate the database"
 python manage.py makemigrations --noinput
 python manage.py migrate --noinput
-python manage.py collectstatic --npinput
+python manage.py collectstatic --noinput
 exec "$@"
