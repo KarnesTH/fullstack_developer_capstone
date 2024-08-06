b065c7e3a50e116fb788a212035c23de1020d147
Patrick Hähnel <p_haehnel@hotmail.de>
2024-08-06 09:55:07 +0200

    edit error handling on searchcars_request

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -70,7 +70,8 @@ def searchcars_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception as e:
+        print(f"Unexpected {e=}, {type(e)=}")
         # If any error occurs
         print("Network exception occurred")
     finally:
